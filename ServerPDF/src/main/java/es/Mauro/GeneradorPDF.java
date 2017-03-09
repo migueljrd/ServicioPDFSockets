85cd918f9300859fedec8f822a1f5faed3c2bc13
Miguel Robledo <migueljrd@hotmail.com>
2017-03-09 15:29:06 +0100

    Servicio PDF updated

---
+++ ServerPDF/src/main/java/es/Mauro/GeneradorPDF.java
@@ -0,0 +1,20 @@
+package es.Mauro;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+public class GeneradorPDF {
+	
+    public void GenerarPDF(String pedido) throws FileNotFoundException, DocumentException {
+      FileOutputStream archivo = new FileOutputStream("C:/Users/miguel/Desktop/pedido.pdf");
+      Document documento = new Document();
+      PdfWriter.getInstance(documento, archivo);
+      documento.open();
+      documento.add(new Paragraph(pedido));
+      documento.close();
+   }
+}
---
+++ ServerPDF/src/main/java/es/Mauro/ServerPdfApplication.java
@@ -0,0 +1,14 @@
+package es.Mauro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPdfApplication.class, args);
+		Servidor x=new Servidor();
+		x.iniciar();
+	}
+}
